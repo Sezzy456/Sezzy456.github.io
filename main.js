e317820fe123dce3a43b15c67fc4180a514639ad
Sezzy456 <92089337+Sezzy456@users.noreply.github.com>
2022-06-09 01:50:38 +1000

    Final touches

--- main.js
+++ main.js
@@ -1,20 +1,30 @@
-document.addEventListener("DOMContentLoader", function(){
-    // Cod to execute when only the HTML document is loaded.
-    // this doesn't wait for stylesheets,
-    // images, and subframes to finish loadin
+// document.addEventListener("DOMContentLoader", function(){
+//     // Cod to execute when only the HTML document is loaded.
+//     // this doesn't wait for stylesheets,
+//     // images, and subframes to finish loadin
 
-    var count = 0;
-    var galleryElements = document.getElementsByClassName('product-tile');
-    Array.prototype.filter.call(galleryElements, funtion(element) {
-        element.textContent = count;
-        console.log(element + count);
-        count++;
-        element.addEventListener("click", funtion(){
-            alert("Heyyyy you have clicked me! number " + this.textContent)
-            this.classList.toggle("selected");
-        })
-    })
-});
+//     var count = 0;
+//     var galleryElements = document.getElementsByClassName('product-tile');
+//     Array.prototype.filter.call(galleryElements, funtion(element) {
+//         element.textContent = count;
+//         console.log(element + count);
+//         count++;
+//         element.addEventListener("click", funtion(){
+//             alert("Heyyyy you have clicked me! number " + this.textContent)
+//             this.classList.toggle("selected");
+//         })
+//     })
+// });
+
+function toggleDisplay(toggle) {
+  var x = document.getElementsByClassName(toggle);
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
 
 function searchCheck() {
   var input;
